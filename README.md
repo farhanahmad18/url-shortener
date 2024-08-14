61e7a5dcd85ddabb13ae859ad5bf614de717fe13
farhanahmad18 <farhan1803ahmad@gmail.com>
2024-08-15 00:39:41 +0530

    Url Shortener Ready to use

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # url-shortener
+To Start the url shortener follow the steps
+1. Install nodejs and mongoDB
+2. Install express and mongoose using npm
+3. Connect the index.js file with the local server of mongoDB
+4. Run the program on Postman or browser by GET or POST method.
---
+++ routes/url.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const { generateNewShortURL, handleGetAnalytics } = require("../controllers/url");
+
+const router = express.Router();
+
+router.post("/", generateNewShortURL);
+
+router.get("/analytics/:shortId", handleGetAnalytics)
+
+module.exports = router;
